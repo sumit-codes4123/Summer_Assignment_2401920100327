03c83b51b824307b434b65ea940e84975bdfda3a
sumit-codes4123 <sumitgarg4123@gmail.com>
2026-06-28 22:16:28 +0530

    Week 4 OOPS solutions

---
+++ Week_4/OOPS/Test_Point.java
@@ -0,0 +1,41 @@
+class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void displayPoint() {
+        System.out.println("Point coordinates: (" + this.x + ", " + this.y + ")");
+    }
+}
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point p = new Point(10, 20);
+        p.displayPoint();
+        
+        p.setXY(50, 60);
+        p.displayPoint();
+    }
+}
